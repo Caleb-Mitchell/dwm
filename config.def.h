92532794a204f1f8618e9208bcbff9663bc87ad4
Caleb-Mitchell <63262011+Caleb-Mitchell@users.noreply.github.com>
2021-08-19 22:52:59 -0500

    Make master/stack layout default

--- config.def.h
+++ config.def.h
@@ -62,8 +62,8 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 #include "fibonacci.c"
 static const Layout layouts[] = {
 	/* symbol     arrange function */
- 	{ "[@]",      spiral },
 	{ "[]=",      tile },    /* first entry is default */
+ 	{ "[@]",      spiral },
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
  	{ "[\\]",      dwindle },
